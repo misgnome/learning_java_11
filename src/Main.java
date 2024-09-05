35a2fa76411f86983034e0a74308b8c4e232ee21
misgnome <misgnome@gmail.com>
2024-09-05 15:27:28 -0500

    introduce decision-making

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Primitive Types
-        int studentAge = 15;
-        double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
-        
-        // Reference Types - built out of organizing primitive types
-        // 1. Strings
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
+       // Decision making
+       System.out.println("Pick a number between 1 and 10:");
+       Scanner scanner = new Scanner(System.in);
 
-        // String access exercises
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
-        
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
+       int inputtedNum = scanner.nextInt();
 
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-
-        //String concatenation
-        String studentName = studentFirstName + " " + studentLastName;
-        System.out.println(studentName + " has a GPA of " + studentGPA + ".");
-        
-        // Interact with and receive user input!
-        System.out.println("What do you want to update it to?");
-        Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
-
-        // Print new output
-        System.out.println(studentName + " now has a GPA of " + studentGPA + ".");
+       if (inputtedNum < 5) {
+        System.out.println("Enjoy the good luck a friend brings you.");
+       } else {
+        System.out.println("Your shoe selection will make you happy today.");
+       }
     }
 }
    
