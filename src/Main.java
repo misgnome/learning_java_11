f8dbe5c2be6ba3fbf4462db0710411f786df74df
misgnome <misgnome@gmail.com>
2024-09-05 14:43:11 -0500

    introduce strings

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,25 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
+        // Primitive Types
         int studentAge = 15;
         double studentGPA = 3.45;
         char studentFirstInitial = 'K';
         char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
         
+        // Reference Types - built out of organizing primitive types
+        // 1. Strings
+        String studentName = "Kayla Hammon";
+
         
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+
+        System.out.println(studentName);
     }
 }
    
