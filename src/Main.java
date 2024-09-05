aefc1c992723b5f3a8e11282d7ef340a77f314a9
misgnome <misgnome@gmail.com>
2024-09-05 15:33:08 -0500

    introduce while loops

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       // Decision making
-       System.out.println("Pick a number between 1 and 10:");
-       Scanner scanner = new Scanner(System.in);
+       // While loops
+       boolean isCurrentSongOnRepeat = true;
+       Scanner input = new Scanner(System.in);
 
-       int inputtedNum = scanner.nextInt();
+       while(isCurrentSongOnRepeat) {
+        System.out.println("Playing current song");
+        System.out.println("Would you like to take this song off of repeat? (y/n)");
+        String userInput = input.next();
 
-       if (inputtedNum < 5) {
-        System.out.println("Enjoy the good luck a friend brings you.");
-       } else {
-        System.out.println("Your shoe selection will make you happy today.");
+        if (userInput.equals("y")) {
+            isCurrentSongOnRepeat = false;
+        }
        }
+       System.out.println("Playing next song");
     }
 }
    
